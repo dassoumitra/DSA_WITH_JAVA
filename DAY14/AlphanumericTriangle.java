2333e5c7f1830a68d78efb033d0a5512ed06cf5e
dassoumitra <soumitrad801@gmail.com>
2026-02-16 22:36:10 +0530

    Day14

---
+++ DAY14/AlphanumericTriangle.java
@@ -0,0 +1,29 @@
+// 1
+// AB
+// 123
+// ABCD
+
+import java.util.Scanner;
+
+public class AlphanumericTriangle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of rows: ");
+    int r = sc.nextInt();
+
+    for(int i=1; i<=r; i++){
+      for(int j=1; j<=i; j++){
+        if(i%2==0){
+          System.out.print((char)(64+j)+" ");
+        }
+        else
+          System.out.print(j+" ");
+
+
+      }
+      System.out.println();
+
+    }
+    sc.close();
+  }
+}
--- DAY14/StarTriangle.java
+++ DAY14/StarTriangle.java
@@ -1,23 +1,20 @@
-// ABC
-// ABC
-// ABC
+// *
+// **
+// ***
 
 import java.util.Scanner;
 
-public class CharacterPrint {
+public class StarTriangle {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter number of rows & columns: ");
+    System.out.print("Enter number of rows: ");
     int r = sc.nextInt();
-    int c = r;
 
     for(int i=1; i<=r; i++){
-      for(int j=1; j<=c; j++){
-        System.out.print((char)(64+j));
+      for(int j=1; j<=i; j++){
+        System.out.print("* ");
       }
       System.out.println();
     }
-    sc.close();
-  
   }
 }
