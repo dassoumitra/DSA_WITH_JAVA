5338a84b69d95534f89272b83721a270e2478f7e
dassoumitra <soumitrad801@gmail.com>
2026-02-16 22:07:30 +0530

    Day14

--- DAY14/NumberSquare.java
+++ DAY14/NumberSquare.java
@@ -1,11 +1,11 @@
-// 1234
-// 1234
-// 1234
-// 1234
+// 1111
+// 2222
+// 3333
+// 4444
 
 import java.util.Scanner;
 
-public class PrintNumber {
+public class NumberSquare {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter number of rows & columns: ");
@@ -14,12 +14,10 @@ public class PrintNumber {
 
     for(int i=1; i<=r; i++){
       for(int j=1; j<=c; j++){
-        System.out.print(j);
+        System.out.print(i+" ");
       }
       System.out.println();
     }
-
     sc.close();
-  
   }
 }
