cea234c0e05deaa4e97d8b63c03e09c4fd88f9a0
dassoumitra <soumitrad801@gmail.com>
2026-02-16 21:56:43 +0530

    Day14

---
+++ DAY14/PrintSquare.java
@@ -0,0 +1,23 @@
+// ****
+// ****
+// ****
+// ****
+
+import java.util.Scanner;
+
+public class PrintSquare {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter rows and columns: ");
+    int r = sc.nextInt();
+    int c = sc.nextInt();
+    
+    for(int i=0; i<r; i++){
+      for(int j=0; j<c; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
