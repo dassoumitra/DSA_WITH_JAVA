0609ca3a8be8eb88038894fb1d2eda843e68a2fd
dassoumitra <soumitrad801@gmail.com>
2026-02-17 13:52:14 +0530

    Day15

--- DAY15/StarPlus.java
+++ DAY15/StarPlus.java
@@ -11,12 +11,14 @@ public class StarPlus {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter number of rows: ");
     int r = sc.nextInt();
+    int mid = Math.ceilDiv(r,2);
+    //System.out.println(mid);
   
 
     for(int i=1; i<=r; i++){
       
       for(int j=1; j<=r; j++){
-        if(i==3 || j==3)
+        if(mid==i || mid==j)
           System.out.print("*");
         else
           System.out.print(" ");
