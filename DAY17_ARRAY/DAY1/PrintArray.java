cf2f37f90515e3ce5c42be5ec139d70eee8a7aa0
dassoumitra <soumitrad801@gmail.com>
2026-02-20 21:24:03 +0530

    Array Day1

---
+++ DAY17_ARRAY/DAY1/PrintArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrintArray {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter size of array: ");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+    System.out.print("Enter element one by one: ");
+    for(int i=0; i<size; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.print("Array is : ");
+    for(int i=0; i<size; i++){
+      System.out.print(arr[i]+" ");
+    }
+
+    sc.close();
+  }
+}
