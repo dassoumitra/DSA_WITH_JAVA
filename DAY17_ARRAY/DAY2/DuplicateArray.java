eab48cb13d41897c1140a266a3479fc67f72cf21
Soumitra Das <125145966+dassoumitra@users.noreply.github.com>
2026-02-27 23:11:08 +0530

    Duplicate element of an array

---
+++ DAY17_ARRAY/DAY2/DuplicateArray.java
@@ -0,0 +1,18 @@
+public class DuplicateArray {
+    public static void main(String[] args) {
+
+        int arr[] = {2, 5, 3, 2, 8, 5};
+
+        System.out.println("Duplicate elements are:");
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                }
+
+            }
+        }
+    }
+}
